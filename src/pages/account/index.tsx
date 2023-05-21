b194cb936fb6426e4af504dbe56787d4dea731da
Stijn Aa <stijn.aa@capgemini.com>
2023-05-21 18:38:02 +0200

    update caseing

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Navigation from "@/components/nav/Nav";
 import SimplePage from "@/components/simplePage/SimplePage";
 import WelcomeMsg from "@/components/welcomeMsg/WelcomeMsg";
-import LogIn from "@/components/login/-login";
+import LogIn from "@/components/login/Login";
 import FriendsList from "@/components/friends/FriendsList";
 
 export default function Index() {
